f373ea0bee13d5110a9a32fefd562e3b964d5c8a
Rafi <martirosyanrafi@gmail.com>
2018-12-11 03:30:44 +0400

    ResearchController

---
+++ Web/Controllers/ResearchController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class ResearchController : Controller
+    {
+        [HttpPost]
+        public string Start([System.Web.Http.FromBody] ResearchBody content)
+        {
+
+            return "Home Page";
+        }
+    }
+}
